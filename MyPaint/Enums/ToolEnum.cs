aa24017766612f9ce4097e058891aecef7d8c581
Zdeněk Mazurák <z.mazurak35@gmail.com>
2019-02-17 14:35:16 +0100

    asd

--- MyPaint/Enums/ToolEnum.cs
+++ MyPaint/Enums/ToolEnum.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyPaint
+﻿namespace MyPaint
 {
     public enum ToolEnum
     {
-        SELECTAREA, SELECT, LINE, POLYLINE, RECT, ELLIPSE, POLYGON, QUADRATICCURVE, TEXT
+        SELECTAREA, SELECT, PENCIL, LINE, POLYLINE, RECT, ELLIPSE, POLYGON, QUADRATICCURVE, TEXT
     }
 }
---
+++ MyPaint/json/Serializer/Pencil.cs
@@ -0,0 +1,15 @@
+﻿namespace MyPaint.Serializer
+{
+    class Pencil : Polygon
+    {
+        public Pencil()
+        {
+            type = "PENCIL";
+        }
+
+        public override Shapes.Shape Create(DrawControl c, MyPaint.Layer la)
+        {
+            return new Shapes.Pencil(c, la, this);
+        }
+    }
+}
